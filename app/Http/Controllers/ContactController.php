89fd626995930f252715d16d126f1ad4b835d1e4
Miriam Araújo <miriamaraujoweb@gmail.com>
2024-08-27 07:50:44 +0100

    trying again sending emails, but now with ssl certificate

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -6,6 +6,9 @@ use Illuminate\Http\Request;
 use App\Mail\ContactFormMail;
 use Illuminate\Support\Facades\Mail;
 use Log;
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 class ContactController extends Controller
 {
     public function index()
@@ -17,33 +20,33 @@ class ContactController extends Controller
     public function submitContactForm(Request $request)
     {
         // Validate the request data
-        /*  $validatedData = $request->validate([
-              'name' => 'required|string|max:255',
-              'email' => 'required|email|max:255',
-              'message' => 'required|string|max:2000',
-          ]);
+        $validatedData = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|max:255',
+            'message' => 'required|string|max:2000',
+        ]);
 
-          // Send the email
-          Mail::to(env('CONTACT_FORM_RECIPIENT'))->send(new ContactFormMail($validatedData));
+        // Send the email
+        Mail::to(env('CONTACT_FORM_RECIPIENT'))->send(new ContactFormMail($validatedData));
 
-          // Redirect back with a success message
-          return redirect()->back()->with('success', 'Your message has been sent successfully!');*/
-
-
-        $to = "miriamaraujoweb@gmail.com";
-        $subject = "Test Email";
-        $message = "This is a test email to verify SMTP settings.";
-        $headers = "From: your_email@yourdomain.com";
-
-        // Test sending email
-        if (mail($to, $subject, $message, $headers)) {
-            Log::info("Email sent successfully.");
-        } else {
-            Log::info("Email failed.");
-            //echo "Failed to send email.";
-        }
-
+        // Redirect back with a success message
         return redirect()->back()->with('success', 'Your message has been sent successfully!');
+
+
+        /*  $to = "miriamaraujoweb@gmail.com";
+          $subject = "Test Email";
+          $message = "This is a test email to verify SMTP settings.";
+          $headers = "From: your_email@yourdomain.com";
+
+          // Test sending email
+          if (mail($to, $subject, $message, $headers)) {
+              Log::info("Email sent successfully.");
+          } else {
+              Log::info("Email failed.");
+              //echo "Failed to send email.";
+          }
+  */
+        //  return redirect()->back()->with('success', 'Your message has been sent successfully!');
     }
 
 
