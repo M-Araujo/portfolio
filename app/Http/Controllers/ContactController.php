7fbe07022a3e674cf7021aec67f520db2c329db4
Miriam Araújo <miriamaraujoweb@gmail.com>
2024-08-27 08:46:32 +0100

    cleaning code

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -19,49 +19,24 @@ class ContactController extends Controller
 
     public function submitContactForm(Request $request)
     {
-        // Validate the request data
         $validatedData = $request->validate([
             'name' => 'required|string|max:255',
             'email' => 'required|email|max:255',
             'message' => 'required|string|max:2000',
         ]);
 
-        // Retrieve the recipient email from the environment variable
-        $recipient = config('mail.from.address');
-        Log::info($recipient);
 
-        // Check if the recipient is set and not empty
+        $recipient = config('mail.from.address');
+
         if (!empty($recipient)) {
-            // Send the email
             Mail::to($recipient)->send(new ContactFormMail($validatedData));
-            Log::info('Email sent successfully!');
+            return redirect()->back()->with('success', 'Your message has been sent successfully!');
         } else {
-            // Handle the case where the recipient is not set
-            Log::info('Error: No recipient email is configured.');
+            Log::info('Error: Error in sending contact form. No recipient email address configured.');
+            return redirect()->back()->with('error', 'There was an error in sending your message. Please try again later.');
         }
 
-        // Redirect back with a success message
-        return redirect()->back()->with('success', 'Your message has been sent successfully!');
 
-
-        /*  $to = "miriamaraujoweb@gmail.com";
-          $subject = "Test Email";
-          $message = "This is a test email to verify SMTP settings.";
-          $headers = "From: your_email@yourdomain.com";
-
-          // Test sending email
-          if (mail($to, $subject, $message, $headers)) {
-              Log::info("Email sent successfully.");
-          } else {
-              Log::info("Email failed.");
-              //echo "Failed to send email.";
-          }
-  */
-        //  return redirect()->back()->with('success', 'Your message has been sent successfully!');
     }
-
-
-
-
 
 }
